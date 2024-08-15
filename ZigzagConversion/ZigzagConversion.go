48b3cb6059428a5c2f5653987446fcfe1bd38d3c
Darren <tn00869679@gmail.com>
2024-08-15 15:04:41 +0800

    refactor : Medium-ZigzagConversion

--- ZigzagConversion/ZigzagConversion.go
+++ ZigzagConversion/ZigzagConversion.go
@@ -1,42 +1,25 @@
 package ZigzagConversion
 
-import "strings"
-
 func ZigzagConversion(s string, numRows int) string {
-	explanationArr := [][]string{}
-	explanationStr := ""
-
-	i := 0
-	for i < len(s) {
-		end := i + numRows
-		if end > len(s) {
-			end = len(s)
-		}
-
-		str := s[i:end]
-		strSlice := strings.Split(str, "")
-		i += numRows
-		explanationArr = append(explanationArr, strSlice)
-		if end == len(s) {
-			break
-		}
-		for j := numRows - 1; j > 1; j-- {
-			stringsSlice := make([]string, numRows)
-			stringsSlice[j-1] = s[i : i+1]
-			i += 1
-			explanationArr = append(explanationArr, stringsSlice)
-			if i > len(s)-1 {
-				break
-			}
-		}
+	if numRows == 1 {
+		return s
 	}
 
-	for n := 0; n < numRows; n++ {
-		for _, slice := range explanationArr {
-			if n > len(slice)-1 {
-				continue
+	if numRows >= len(s) {
+		return s
+	}
+
+	explanationStr := ""
+	for i := 0; i < numRows; i++ {
+		explanationStr += s[i : i+1]
+		for j := i + (2 * (numRows - 1)); j-(2*i) < len(s); j += (2 * (numRows - 1)) {
+			if i != 0 && i != numRows-1 {
+				explanationStr += s[j-(2*i) : j-(2*i)+1]
 			}
-			explanationStr += slice[n]
+
+			if j < len(s) {
+				explanationStr += s[j : j+1]
+			}
 		}
 	}
 
